e86b0ef7e42d293b6f0d9c1d4c6f46f0b70e6dc4
1753589103 <73995938+1753589103@users.noreply.github.com>
2022-09-09 21:20:08 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,38 +6,38 @@ const CONFIG = {
   // ================================高级配置 - 需要修改================================
 
   // 微信公众号的appID
-  APP_ID: "",
+  APP_ID: "wx380713899c9e171e",
 
   // 微信公众号的appsecret
-  APP_SECRET: "",
+  APP_SECRET: "90412c352abd8b610870e8c53aa77243",
 
   // 微信公众号的模板ID
-  TEMPLATE_ID: "",
+  TEMPLATE_ID: "Vb4PwnEDnzSH8wJhuVwzwjRlstlJmu1CbxLKeroGLG0",
 
   // 天行API的key
-  TXApiKey: "",
+  TXApiKey: "387c47aaece63dd6850ff11889e3b30a",
 
   // 用户列表 可配置多个用户
   user: [
     {
       // 男(女)朋友的名字（或昵称或网名）
-      userName: "小沐沐吖",
+      userName: "曦曦宝贝",
       // 用户列表的`微信号`
-      userId: "",
+      userId: "oA6Rv5xlwYdIYX4X_vAmwALv1tPw",
       // 星座
-      star: "摩羯座",
+      star: "双鱼座",
       // 生日 - 格式：08-22
-      birthday: "01-28",
+      birthday: "01-29",
       // 城市 - 格式：支持省市县区
-      city: "夏邑县",
+      city: "成都市",
     },
   ],
 
   // 计划旅游日 - 格式：2022-05-20
-  tourism: "2022-12-12",
+  tourism: "2022-10-1",
 
   // 恋爱纪念日 - 格式：2022-05-20
-  loveDay: "2022-05-20",
+  loveDay: "2019-03-17",
 
   // ================================功能配置 - 可选修改================================
 
